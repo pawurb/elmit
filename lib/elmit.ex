27ba82e999e464b1797cc6e67db03201ffbeec1b
pawurb <p.urbanek89@gmail.com>
2015-11-26 15:13:05 +0100

    fetching google data on the basis of args

--- lib/elmit.ex
+++ lib/elmit.ex
@@ -2,9 +2,22 @@ require HTTPotion
 require Logger
 
 defmodule Elmit do
-  def main(_args) do
+
+  def main(args) do
     IO.puts "Hello elmit!"
-    HTTPotion.get("http://dupa.pl") |> handle_response
+    opts = parse_args(args)
+
+    opts
+    |> construct_url
+    |> issue_request
+    |> handle_response
+  end
+
+  defp parse_args(args) do
+    {options, _, _} = OptionParser.parse(args,
+      switches: [from: :string, to: :string, text: :string]
+    )
+    options
   end
 
   # def request(id, url) do
@@ -16,8 +29,19 @@ defmodule Elmit do
   #   end
   # end
 
+  defp construct_url(opts) do
+    host = "https://translate.google.com"
+    url = "#{host}/translate_a/single?client=t&sl=#{opts[:from]}&tl=#{opts[:to]}&hl=pl&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&dt=at&ie=UTF-8&oe=UTF-8&otf=2&srcrom=1&ssel=3&tsel=6&kc=2&tk=522578&q=#{opts[:text]}"
+    IO.puts url
+    url
+  end
+
+  defp issue_request(url) do
+    HTTPotion.get(url)
+  end
+
   defp handle_response(response) do
-    IO.puts "#{response.body}: success"
+    IO.puts "#{response.body}"
   end
 
   # defp handle_response(%HTTPoison.Response{status_code: status_code}, id) do
