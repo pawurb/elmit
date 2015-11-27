be318ea705288edf521eeca9b0c43189455bea29
pawurb <p.urbanek89@gmail.com>
2015-11-27 12:51:57 +0100

    opts filtering and pattern matching

--- lib/elmit.ex
+++ lib/elmit.ex
@@ -7,6 +7,7 @@ defmodule Elmit do
     opts = parse_args(args)
 
     response = opts
+    |> Enum.filter(fn(x) -> Enum.member?([:from, :to, :text], elem(x, 0)) end)
     |> build_text_url
     |> fetch
 
@@ -20,8 +21,9 @@ defmodule Elmit do
     IO.puts "=> #{translation} #{synonyms}"
 
     if opts[:t] do
-      sound_opts = List.keyreplace(opts, :text, 0, {:text, translation})
-      sound_opts
+      opts
+      |> Enum.filter(fn(x) -> Enum.member?([:to, :text], elem(x, 0)) end)
+      |> List.keyreplace(:text, 0, {:text, translation})
       |> build_sound_url
       |> fetch
       |> handle_sound_response
@@ -94,12 +96,12 @@ ELMIT: Wrong data. Example: 'elmit en es the cowboy' => 'el vaquero'
     options
   end
 
-  defp build_text_url(opts) do
-    "#{@host}/translate_a/single?client=t&sl=#{opts[:from]}&tl=#{opts[:to]}&hl=pl&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&dt=at&ie=UTF-8&oe=UTF-8&otf=2&srcrom=1&ssel=3&tsel=6&kc=2&tk=522578&q=#{URI.encode(opts[:text])}"
+  defp build_text_url([from: from, to: to, text: text]) do
+    "#{@host}/translate_a/single?client=t&sl=#{from}&tl=#{to}&hl=pl&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&dt=at&ie=UTF-8&oe=UTF-8&otf=2&srcrom=1&ssel=3&tsel=6&kc=2&tk=522578&q=#{URI.encode(text)}"
   end
 
-  defp build_sound_url(opts) do
-    "#{@host}/translate_tts?ie=UTF-8&tl=#{opts[:to]}&total=1&idx=0&textlen=5&tk=735012&client=t&q=#{URI.encode(opts[:text])}"
+  defp build_sound_url([to: to, text: text]) do
+    "#{@host}/translate_tts?ie=UTF-8&tl=#{to}&total=1&idx=0&textlen=5&tk=735012&client=t&q=#{URI.encode(text)}"
   end
 
   defp extract_translation(%HTTPotion.Response{body: body}) do
