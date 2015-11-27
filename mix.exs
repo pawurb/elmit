6d50590dc6f9ff88cfba2dcb2ce2adb14314f2a9
pawurb <p.urbanek89@gmail.com>
2015-11-27 11:59:43 +0100

    metadata

--- mix.exs
+++ mix.exs
@@ -2,11 +2,14 @@ defmodule Elmit.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :elmit,
-     version: "0.0.1",
-     elixir: "~> 1.1.0",
-     escript: [main_module: Elmit],
-     deps: deps]
+    [
+      app: :elmit,
+      version: "0.0.1",
+      elixir: "~> 1.1.0",
+      escript: [main_module: Elmit],
+      description: description,
+      deps: deps
+    ]
   end
 
   def application do
